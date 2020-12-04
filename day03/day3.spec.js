4a38ca1604a4b66fa8a240435c77fa3976a70e76
Marcel van den Bosch <m.v.d.bosch@gmail.com>
2020-12-04 14:44:47 +0100

    Fix day 3 test

--- day03/day3.spec.js
+++ day03/day3.spec.js
@@ -4,7 +4,7 @@ const path = require('path');
 const part1 = require('./part1');
 const part2 = require('./part2');
 
-describe('day 1', () => {
+describe('day 3', () => {
   describe('part 1', () => {
     it('should return the correct value with test values', () => {
       const input = `..##.......
